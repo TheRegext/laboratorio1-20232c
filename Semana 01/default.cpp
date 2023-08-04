035e55f7db62c9d77a277c3f41bffd7babd9fb6d
Brian Lara <brian.lara@outlook.com.ar>
2023-08-04 10:57:46 -0300

    inicial

---
+++ Semana 01/default.cpp
@@ -0,0 +1,19 @@
+///Ejercicio: 
+///Autor: 
+///Fecha:
+///Comentario:
+
+# include<iostream>
+using namespace std;
+
+int main(){
+    // Aca van las declaraciones
+		
+	// Aca van los datos de entrada
+		
+	// Aca van las operaciones
+		
+	// Aca van los datos de salida
+	
+	return 0;
+}
---
+++ Semana 01/suma.cpp
@@ -0,0 +1,22 @@
+///Ejercicio: suma de dos números
+///Autor:DEK
+///Fecha:04/08/2023
+///Comentario:primer ejercicio para el curso de los viernes. Suma de dos números
+
+# include<iostream>
+
+using namespace std;
+
+
+int main(){
+    int n1, n2,suma;
+    cout<<"INGRESE UN NUMERO ";
+	cin>>n1;
+	cout<<"INGRESE UN NUMERO ";
+	cin>>n2;
+	suma=n1+n2;
+	cout<<"LA SUMA ES ";
+	cout<<suma<<endl<<endl;
+	system("pause>null");
+	return 0;
+}
